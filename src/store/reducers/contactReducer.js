35d9f533908f9413021184292917fc61c50ea937
shaniHer <81388030+shaniHer@users.noreply.github.com>
2022-11-17 10:20:58 +0200

    Update contactReducer.js

--- src/store/reducers/contactReducer.js
+++ src/store/reducers/contactReducer.js
@@ -1,10 +1,3 @@
-//implementing state management: 1. writing the reducer function 2. creating a store object (store/index.js)
-//3. introducing the store to the app via provider in the src/index.js
-//4. write a connect func in eac cmp that uses the store
-//5. write mapStateToProps func to declare which state part to use in the cmp
-//6. create actions dispatcher layer, create dispatch functions
-//7. create mapDispatchToProps obj in each cmp and use the dispatch action
-//8. join reducers into combined in index.js
 
 const INITIAL_STATE = {
   contacts: null,
